dc78313ce186c6ef921988b47c63c20c4675c59a
jdalvarez <73964445+jdalvarez@users.noreply.github.com>
2021-07-06 18:09:52 -0300

    Recycler View partial implementation

--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -4,7 +4,7 @@ import com.example.affirmations.R
 import com.example.affirmations.model.Affirmation
 
 class Datasource {
-    fun loadAffirmation(): List<Affirmation>{
+    fun loadAffirmations(): List<Affirmation>{
         return  listOf<Affirmation>(
         Affirmation(R.string.affirmation1),
         Affirmation(R.string.affirmation2),
