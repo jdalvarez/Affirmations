21dee0f8a966b7a69dd4f69dd2bd290980cca0f0
jdalvarez <73964445+jdalvarez@users.noreply.github.com>
2021-07-08 12:28:12 -0300

    RecyclerView test

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -2,28 +2,27 @@ package com.example.affirmations.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.affirmations.R
+import com.example.affirmations.databinding.ListItemBinding
 import com.example.affirmations.model.Affirmation
 
 class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
     RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.item_title)
+    class ItemViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_item, parent, false)
-        return ItemViewHolder(itemView)
+        val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ItemViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        holder.textView.text = context.getString(dataset[position].stringResourceId)
+        val item = dataset[position]
+        holder.binding.itemTitle.text = context.resources.getString(item.stringResourceId)
+        holder.binding.itemImage.setImageResource(item.imageResourcedId)
     }
 
     override fun getItemCount() = dataset.size
--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,6 +1,12 @@
 package com.example.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 //Esto es una clase de datos
-data class Affirmation (val stringResourceId:Int) {
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourcedId: Int
+) {
 
 }
